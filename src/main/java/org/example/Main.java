182a54e455f3bbcac5ea964c001f4ad6872ce960
TomJozz <TomJozz@users.noreply.github.com>
2025-10-22 12:34:44 +0200

    Add Functions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        String content = DigitReader.readFileAsString("./hyperskill-dataset.txt");
+
 
     }
-
-
 }
 
 class DigitReader {
@@ -34,5 +36,15 @@ class DigitCounter {
             }
         }
         return frequencyMap;
+    }
+}
+
+class TopDigitsFinder {
+    public static List<Map.Entry<Character, Integer>> findTopNDigits(Map<Character, Integer> frequencyMap, int n) {
+        return frequencyMap.entrySet()
+                .stream()
+                .sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+                .limit(n)
+                .collect(Collectors.toList());
     }
 }
